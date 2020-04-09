fc09ca2678925a065bfdd4d09f94f31b2824a4df
martins20 <pdesigner@gmail.com>
2020-04-09 19:14:46 -0300

    first commit: Created a pipefy clone using react js version 1.0. Features: drag in drop

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  h1 {
+    height: 50px;
+    padding: 0 30px;
+    background: #7159c1;
+    color: #fff;
+    font-size: 2em;
+
+    display: flex;
+    align-items: center;
+  }
+`;
